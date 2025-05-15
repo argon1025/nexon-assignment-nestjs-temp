48b311ce7479b53741dff277237b5eee6dade66d
argon1025 <argon1025@gmail.com>
2025-05-15 23:29:37 +0900

    feat: Swagger 구성

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -13,7 +14,21 @@ async function bootstrap() {
   const PORT = configService.getOrThrow<string>('PORT');
   const ENV = configService.getOrThrow<string>('NODE_ENV');
 
+  // Swagger
+  if (ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle(SERVICE_NAME)
+      .setDescription(`${SERVICE_NAME} Service API Docs`)
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api', app, document, {
+      customSiteTitle: `${SERVICE_NAME} Service API Docs`,
+    });
+  }
+
   await app.listen(PORT);
   Logger.log(`${SERVICE_NAME} is running on port ${PORT} in ${ENV}`);
+  Logger.log(`API Docs: http://localhost:${PORT}/api`);
 }
 bootstrap();
